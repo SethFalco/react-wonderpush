3df682635d2198ba9a4fb526e2514fad8bb2e6fb
Seth Falco <seth@falco.fun>
2023-06-27 01:02:54 +0100

    fix: use lifecycle hook

--- src/WonderPushComponent.js
+++ src/WonderPushComponent.js
@@ -1,9 +1,11 @@
-import React,{Component} from 'react';
+import {Component} from 'react';
 import WonderPush from './WonderPush';
 
 export default class WonderPushComponent extends Component {
   constructor(props){
     super(props);
+  }
+  componentDidMount() {
     WonderPush.push(['init', {...this.props.options, integrator: 'react-wonderpush-1.0.9'}]);
   }
   render() {
